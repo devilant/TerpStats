3e41692ab476fb664d59135f8c8dbc041a8ab43f
Dan <devilant@ubuntuVM.(none)>
2014-01-07 16:23:01 -0800

    Updating index page, moving sorttable.js to static folder

--- TerpLineupStats_project/LineupStats/views.py
+++ TerpLineupStats_project/LineupStats/views.py
@@ -1,11 +1,41 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from LineupStats.models import *
 
 def index(request):
 	context = RequestContext(request)
 
-	context_dict = {'boldmessage': "I am bold font from the context"}
+	games = Game.objects.order_by('-date')
+	gamesCount = len(games)
+	#get the most recently played game (to show how current the data is)
+	latestGame = games[0]
+
+	rawLineupData = LineupStats.objects.all()
+
+	#sum the data per-lineup
+	totalLineupData = {}
+	for lineupStat in rawLineupData:
+		lineup = lineupStat.lineup
+		if lineup in totalLineupData:
+			totalLineupData[lineup] += lineupStat
+		else:
+			totalLineupData[lineup] = lineupStat
+
+	sortedLineupStats = sorted(totalLineupData.items(), key=lambda statsDict: statsDict[1].elapsedTime)
+	sortedLineupStats.reverse()
+	lineupStatsList = [lineupStats[1] for lineupStats in sortedLineupStats]
+
+	tableHeaders = ['Lineup', 'Time on Court', 'Possessions', 'Points Per Possession', 'Opponent Points Per Possession', 'Efficiency Margin']
+	data = []
+	for lineupStats in lineupStatsList:
+		dataRow = [lineupStats.lineup, lineupStats.getElapsedTime(), lineupStats.possessionCount, lineupStats.getPointsPerPossession(), lineupStats.getOppPointsPerPossession(), lineupStats.getEfficiencyMargin()]
+		data.append(dataRow)		
+
+	context_dict = {'gamesCount': gamesCount,
+					'latestGame': latestGame,
+					'tableHeaders': tableHeaders,
+					'data': data}
 
 	return render_to_response('LineupStats/index.html', context_dict, context)
 
