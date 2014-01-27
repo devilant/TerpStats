c6351837b513318fdcc9ebfb2ab6ca1127c16f5b
Dan <devilant@ubuntuVM.(none)>
2014-01-27 09:17:33 -0800

    Updating LineupFilterForm.py

--- TerpLineupStats_project/LineupStats/LineupFilterForm.py
+++ TerpLineupStats_project/LineupStats/LineupFilterForm.py
@@ -18,6 +18,25 @@ class LineupFilterForm(forms.Form):
 	 	('Spencer Barks', 'Spencer Barks'),
 	 	('Varun Ram', 'Varun Ram'),	 	
 	 	)
+
+	statTypes = (
+		('Time on Court', 'Time on Court'),
+		('Possessions', 'Possessions'),
+		('Points Per Possession', 'Points Per Possession'),
+		('Opponent Points Per Possession', 'Opponent Points Per Possession'),
+		('Efficiency Margin', 'Efficiency Margin'),
+		('2p%', '2p%'),
+		('3p%', '3p%'),
+		('Opp 2p%', 'Opp 2p%'),
+		('Opp 3p%', 'Opp 3p%'),
+		('Def Reb %', 'Def Reb %'),
+		('Off Reb %', 'Off Reb %'),
+		('Steal %', 'Steal %'),
+		('Assist %', 'Assist %'),
+		('Turnover %', 'Turnover %'),		
+		)
+	initialStatTypes= ['Time on Court', 'Possessions', 'Points Per Possession', 'Opponent Points Per Possession', 'Efficiency Margin']
+
 	includePlayers = forms.MultipleChoiceField(
 		widget=forms.CheckboxSelectMultiple, 
 		choices = players,
@@ -33,3 +52,9 @@ class LineupFilterForm(forms.Form):
 		max_value=1000,
 		initial=0,
 		label='Show only lineups that have played at least this many possessions:')
+	statsToShow = forms.MultipleChoiceField(
+		widget=forms.CheckboxSelectMultiple,
+		choices = statTypes,
+		required=True,
+		initial=initialStatTypes,
+		label='Select the statistics to show:')
