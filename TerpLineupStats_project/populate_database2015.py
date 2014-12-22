6f10e8504f1ffd5670f4232372eccc60cb052f2e
Dan <devilant@ubuntuVM.(none)>
2014-12-22 04:31:54 -0800

    update data pop script for ooc games 2015

--- TerpLineupStats_project/populate_database2015.py
+++ TerpLineupStats_project/populate_database2015.py
@@ -5,8 +5,32 @@ md = Team.objects.get(name="Maryland")
 
 #create team
 wagner = createTeam("Wagner", "NE")
+monmouth = createTeam("Monmouth", "MAAC")
+uscupstate = createTeam("USC Upstate", "ASun")
+vmi = createTeam("VMI", "Southern")
+virginia = Team.objects.get(name="Virginia")
 
 #wagner game
 date = datetime.datetime(2014, 11, 14, 19, 30)
 game = createGame(md, wagner, date, False, False)
 createLineupStatsForGame(game, md, 'terps2014-2015/wagner.html')
+
+#monmouth game
+date = datetime.datetime(2014, 11, 28, 19, 0)
+game = createGame(md, monmouth, date, False, False)
+createLineupStatsForGame(game, md, 'terps2014-2015/monmouth.html')
+
+#vmi game
+date = datetime.datetime(2014, 11, 30, 18, 0)
+game = createGame(md, vmi, date, False, False)
+createLineupStatsForGame(game, md, 'terps2014-2015/vmi.html')
+
+#virginia game
+date = datetime.datetime(2014, 12, 3, 21, 15)
+game = createGame(md, virginia, date, False, False)
+createLineupStatsForGame(game, md, 'terps2014-2015/virginia.html')
+
+#usc upstate game
+date = datetime.datetime(2014, 12, 13, 11, 0)
+game = createGame(md, uscupstate, date, False, False)
+createLineupStatsForGame(game, md, 'terps2014-2015/uscupstate.html')
