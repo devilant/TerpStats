fe1055438e37dcff8a8929d07e71fe8c278fb17d
Dan <devilant@ubuntuVM.(none)>
2015-11-06 20:25:12 -0800

    Updating pop script for gtown game

--- TerpLineupStats_project/populate_database2016.py
+++ TerpLineupStats_project/populate_database2016.py
@@ -4,8 +4,14 @@ from TerpStats import *
 #create teams
 md = createTeam("Maryland", "B10")
 msm = createTeam("Mount St. Mary's", "NEC")
+gtown = createTeam("Georgetown", "BE")
 
 #mount st. mary's game
 date = datetime.datetime(2015, 11, 13, 19, 0)
 game = createGame(md, msm, date, False, False)
 createLineupStatsForGame(game, md, 'terps2015-2016/mountstmarys.html')
+
+#georgetown game
+date = datetime.datetime(2015, 11, 17, 21, 0)
+game = createGame(md, gtown, date, False, False)
+createLineupStatsForGame(game, md, 'terps2015-2016/georgetown.html')
