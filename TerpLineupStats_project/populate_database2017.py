5b88be61191bbc48bb6b60d49c581526da9afe21
Dan <devilant@ubuntuVM.(none)>
2016-11-07 10:02:06 -0800

    Update pop script for georgetown game, fix parsePlaye to handle L.G. Gill

--- TerpLineupStats_project/populate_database2017.py
+++ TerpLineupStats_project/populate_database2017.py
@@ -3,3 +3,14 @@ from TerpStats import *
 
 md = Team.objects.get(name="Maryland")
 american = createTeam("American", "Patriot")
+georgetown = Team.objects.get(name="Georgetown")
+
+#american game
+date = datetime.datetime(2016, 11, 11, 19, 0)
+game = createGame(md, american, date, False, False)
+createLineupStatsForGame(game, md, 'terps2016-2017/american.html')
+
+#georgetown game
+date = datetime.datetime(2016, 11, 16, 18, 30)
+game = createGame(georgetown, md, date, False, False)
+createLineupStatsForGame(game, md, 'terps2016-2017/georgetown.html')
